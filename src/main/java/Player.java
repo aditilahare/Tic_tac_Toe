4337740f11ff93328e68adfa2180afc26f7eebe6
aditilahare <aditil@thoughtworks.com>
2018-03-26 00:46:20 +0530

    Added getMoves method in Player.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+
 public class Player {
     private final String name;
+    protected ArrayList<Number> moves;
 
     public Player(String name) {
-
         this.name = name;
+        this.moves = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public ArrayList<Number> getMoves() {
+        return this.moves;
+    }
+
+    public void addMove(int move) {
+        moves.add(move);
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,5 +9,12 @@ public class PlayerTest {
     public void checkForPlayerName() {
         Player player1 = new Player("Aditi");
         assertThat(player1.getName(),is("Aditi"));
+    }
+
+    @Test
+    public void checkForMoves() {
+        Player player2 = new Player("Dhanashri");
+        player2.addMove(1);
+        assertThat(player2.getMoves(),hasItem(1));
     }
 }
